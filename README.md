9c8cdc3e062fe551a859a70476cdca174c9025e5
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2024-10-23 17:30:31 -0700

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,50 @@
+
+> Open this page at [https://quest-and-vcs-rnd.github.io/robot100-blank_template-24-1023-1700/](https://quest-and-vcs-rnd.github.io/robot100-blank_template-24-1023-1700/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/quest-and-vcs-rnd/robot100-blank_template-24-1023-1700** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/quest-and-vcs-rnd/robot100-blank_template-24-1023-1700** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
+
+
+
+> Open this page at [https://quest-and-vcs-rnd.github.io/robot100-test-auto_turns-24-1023-1750-v21-010-24-1023-1700/](https://quest-and-vcs-rnd.github.io/robot100-test-auto_turns-24-1023-1750-v21-010-24-1023-1700/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/quest-and-vcs-rnd/robot100-test-auto_turns-24-1023-1750-v21-010-24-1023-1700** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/quest-and-vcs-rnd/robot100-test-auto_turns-24-1023-1750-v21-010-24-1023-1700** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,228 @@
+input.onButtonPressed(Button.A, function () {
+    if (true) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_20,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        basic.showLeds(`
+            # # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_40,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        basic.showLeds(`
+            # # # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_60,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        basic.showLeds(`
+            # # # # .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_80,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (true) {
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_1000,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            # # . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_2000,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        basic.showLeds(`
+            # # # . .
+            # # # . .
+            # # # . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_4000,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        basic.showLeds(`
+            # # # # .
+            # # # # .
+            # # # # .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_5000,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (true) {
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_100,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_200,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        basic.showLeds(`
+            # # # . .
+            # # # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_400,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        basic.showLeds(`
+            # # # # .
+            # # # # .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_800,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+})
+basic.showIcon(IconNames.Heart)
+quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+	
+})
