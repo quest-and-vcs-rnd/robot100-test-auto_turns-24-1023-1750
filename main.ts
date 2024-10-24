1c712a3b59b83499b904714c03b1c8e7296f5cc5
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2024-10-23 18:48:40 -0700

    24-1023-1900 Add Left-Turns to Test both Servo-Motors

--- main.ts
+++ main.ts
@@ -22,6 +22,27 @@ input.onButtonPressed(Button.A, function () {
     }
     if (true) {
         quest_Note_1.quest_Show_String_For_Note_Small_Func(
+        "Battery 75%+, ~ 1x1=1 degrees"
+        )
+        basic.showLeds(`
+            . # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_20,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        quest_Note_1.quest_Show_String_For_Note_Small_Func(
         "Battery 75%+, ~ 2x1=2 degrees"
         )
         basic.showLeds(`
@@ -35,6 +56,27 @@ input.onButtonPressed(Button.A, function () {
         quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
         quest_Turn_Type_Enum.Pivot,
         quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_40,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        quest_Note_1.quest_Show_String_For_Note_Small_Func(
+        "Battery 75%+, ~ 2x1=2 degrees"
+        )
+        basic.showLeds(`
+            . # . . .
+            . # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
         quest_Turn_Power_Enum.Lo,
         quest_Turn_Duration_Enum.msec_40,
         quest_Debug_Show_Enum.Dashboard_OLED
@@ -64,6 +106,27 @@ input.onButtonPressed(Button.A, function () {
     }
     if (true) {
         quest_Note_1.quest_Show_String_For_Note_Small_Func(
+        "Battery 75%+, ~ 3x1=3 degrees"
+        )
+        basic.showLeds(`
+            . # . . .
+            . # . . .
+            . # . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_60,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        quest_Note_1.quest_Show_String_For_Note_Small_Func(
         "Battery 75%+, ~ 4x1=4 degrees"
         )
         basic.showLeds(`
@@ -77,6 +140,27 @@ input.onButtonPressed(Button.A, function () {
         quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
         quest_Turn_Type_Enum.Pivot,
         quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_80,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        quest_Note_1.quest_Show_String_For_Note_Small_Func(
+        "Battery 75%+, ~ 4x1=4 degrees"
+        )
+        basic.showLeds(`
+            . # . . .
+            . # . . .
+            . # . . .
+            . # . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
         quest_Turn_Power_Enum.Lo,
         quest_Turn_Duration_Enum.msec_80,
         quest_Debug_Show_Enum.Dashboard_OLED
@@ -108,6 +192,27 @@ input.onButtonPressed(Button.AB, function () {
     }
     if (true) {
         quest_Note_1.quest_Show_String_For_Note_Small_Func(
+        "Battery 75%+, ~ 1x45=45 degrees"
+        )
+        basic.showLeds(`
+            . # # # .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_1000,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        quest_Note_1.quest_Show_String_For_Note_Small_Func(
         "Battery 75%+, ~ 2x45=90 degrees"
         )
         basic.showLeds(`
@@ -121,6 +226,27 @@ input.onButtonPressed(Button.AB, function () {
         quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
         quest_Turn_Type_Enum.Pivot,
         quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_2000,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        quest_Note_1.quest_Show_String_For_Note_Small_Func(
+        "Battery 75%+, ~ 2x45=90 degrees"
+        )
+        basic.showLeds(`
+            . # # # .
+            . # # # .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
         quest_Turn_Power_Enum.Lo,
         quest_Turn_Duration_Enum.msec_2000,
         quest_Debug_Show_Enum.Dashboard_OLED
@@ -150,6 +276,27 @@ input.onButtonPressed(Button.AB, function () {
     }
     if (true) {
         quest_Note_1.quest_Show_String_For_Note_Small_Func(
+        "Battery 75%+, ~ 3x45=135 degrees"
+        )
+        basic.showLeds(`
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_3000,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        quest_Note_1.quest_Show_String_For_Note_Small_Func(
         "Battery 75%+, ~ 4x45=180 degrees"
         )
         basic.showLeds(`
@@ -171,6 +318,27 @@ input.onButtonPressed(Button.AB, function () {
     }
     if (true) {
         quest_Note_1.quest_Show_String_For_Note_Small_Func(
+        "Battery 75%+, ~ 4x45=180 degrees"
+        )
+        basic.showLeds(`
+            . # # # .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_4000,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        quest_Note_1.quest_Show_String_For_Note_Small_Func(
         "Battery 75%+, ~ 5x45=225 degrees"
         )
         basic.showLeds(`
@@ -184,6 +352,27 @@ input.onButtonPressed(Button.AB, function () {
         quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
         quest_Turn_Type_Enum.Pivot,
         quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_5000,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        quest_Note_1.quest_Show_String_For_Note_Small_Func(
+        "Battery 75%+, ~ 5x45=225 degrees"
+        )
+        basic.showLeds(`
+            . # # # .
+            . # # # .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
         quest_Turn_Power_Enum.Lo,
         quest_Turn_Duration_Enum.msec_5000,
         quest_Debug_Show_Enum.Dashboard_OLED
@@ -215,6 +404,27 @@ input.onButtonPressed(Button.B, function () {
     }
     if (true) {
         quest_Note_1.quest_Show_String_For_Note_Small_Func(
+        "Battery 75%+, ~ 1x5=5 degrees"
+        )
+        basic.showLeds(`
+            . # # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_100,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        quest_Note_1.quest_Show_String_For_Note_Small_Func(
         "Battery 75%+, ~ 2x5=10 degrees"
         )
         basic.showLeds(`
@@ -228,6 +438,27 @@ input.onButtonPressed(Button.B, function () {
         quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
         quest_Turn_Type_Enum.Pivot,
         quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_200,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        quest_Note_1.quest_Show_String_For_Note_Small_Func(
+        "Battery 75%+, ~ 2x5=10 degrees"
+        )
+        basic.showLeds(`
+            . # # . .
+            . # # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
         quest_Turn_Power_Enum.Lo,
         quest_Turn_Duration_Enum.msec_200,
         quest_Debug_Show_Enum.Dashboard_OLED
@@ -257,6 +488,27 @@ input.onButtonPressed(Button.B, function () {
     }
     if (true) {
         quest_Note_1.quest_Show_String_For_Note_Small_Func(
+        "Battery 75%+, ~ 4x5=20 degrees"
+        )
+        basic.showLeds(`
+            . # # . .
+            . # # . .
+            . # # . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_400,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        quest_Note_1.quest_Show_String_For_Note_Small_Func(
         "Battery 75%+, ~ 8x5=40 degrees"
         )
         basic.showLeds(`
@@ -270,6 +522,27 @@ input.onButtonPressed(Button.B, function () {
         quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
         quest_Turn_Type_Enum.Pivot,
         quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_800,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        quest_Note_1.quest_Show_String_For_Note_Small_Func(
+        "Battery 75%+, ~ 8x5=40 degrees"
+        )
+        basic.showLeds(`
+            . # # . .
+            . # # . .
+            . # # . .
+            . # # . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
         quest_Turn_Power_Enum.Lo,
         quest_Turn_Duration_Enum.msec_800,
         quest_Debug_Show_Enum.Dashboard_OLED
