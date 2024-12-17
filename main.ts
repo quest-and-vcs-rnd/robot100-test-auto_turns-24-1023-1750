5ebf37b7837293097e1047af990d92cc9f22a5b8
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2024-12-17 01:58:49 -0800

    robot100-test-auto_turns-2.1 (24-1217-0200): Add 90deg and 180deg Sample

--- main.ts
+++ main.ts
@@ -171,7 +171,7 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.AB, function () {
     if (true) {
         quest_Note_1.quest_Show_String_For_Note_Small_Func(
-        "Battery 75%+, ~ 1x45=45 degrees"
+        "Battery 75%+, ~ 1x45=45 (1x50=50) degrees"
         )
         basic.showLeds(`
             . # # # .
@@ -192,7 +192,7 @@ input.onButtonPressed(Button.AB, function () {
     }
     if (true) {
         quest_Note_1.quest_Show_String_For_Note_Small_Func(
-        "Battery 75%+, ~ 1x45=45 degrees"
+        "Battery 75%+, ~ 1x45=45 (1x50=50) degrees"
         )
         basic.showLeds(`
             . # # # .
@@ -213,7 +213,7 @@ input.onButtonPressed(Button.AB, function () {
     }
     if (true) {
         quest_Note_1.quest_Show_String_For_Note_Small_Func(
-        "Battery 75%+, ~ 2x45=90 degrees"
+        "Battery 75%+, ~ 2x45=90 (2x50=100) degrees"
         )
         basic.showLeds(`
             . # # # .
@@ -234,7 +234,7 @@ input.onButtonPressed(Button.AB, function () {
     }
     if (true) {
         quest_Note_1.quest_Show_String_For_Note_Small_Func(
-        "Battery 75%+, ~ 2x45=90 degrees"
+        "Battery 75%+, ~ 2x45=90 (2x50=100) degrees"
         )
         basic.showLeds(`
             . # # # .
@@ -255,7 +255,7 @@ input.onButtonPressed(Button.AB, function () {
     }
     if (true) {
         quest_Note_1.quest_Show_String_For_Note_Small_Func(
-        "Battery 75%+, ~ 3x45=135 degrees"
+        "Battery 75%+, ~ 3x45=135 (3x50=150) degrees"
         )
         basic.showLeds(`
             . # # # .
@@ -276,7 +276,7 @@ input.onButtonPressed(Button.AB, function () {
     }
     if (true) {
         quest_Note_1.quest_Show_String_For_Note_Small_Func(
-        "Battery 75%+, ~ 3x45=135 degrees"
+        "Battery 75%+, ~ 3x45=135 (3x50=150) degrees"
         )
         basic.showLeds(`
             . # # # .
@@ -297,7 +297,7 @@ input.onButtonPressed(Button.AB, function () {
     }
     if (true) {
         quest_Note_1.quest_Show_String_For_Note_Small_Func(
-        "Battery 75%+, ~ 4x45=180 degrees"
+        "Battery 75%+, ~ 4x45=180 (4x50=200) degrees"
         )
         basic.showLeds(`
             . # # # .
@@ -318,7 +318,7 @@ input.onButtonPressed(Button.AB, function () {
     }
     if (true) {
         quest_Note_1.quest_Show_String_For_Note_Small_Func(
-        "Battery 75%+, ~ 4x45=180 degrees"
+        "Battery 75%+, ~ 4x45=180 (4x50=200) degrees"
         )
         basic.showLeds(`
             . # # # .
@@ -339,7 +339,7 @@ input.onButtonPressed(Button.AB, function () {
     }
     if (true) {
         quest_Note_1.quest_Show_String_For_Note_Small_Func(
-        "Battery 75%+, ~ 5x45=225 degrees"
+        "Battery 75%+, ~ 5x45=225 (5x50=250) degrees"
         )
         basic.showLeds(`
             . # # # .
@@ -360,7 +360,7 @@ input.onButtonPressed(Button.AB, function () {
     }
     if (true) {
         quest_Note_1.quest_Show_String_For_Note_Small_Func(
-        "Battery 75%+, ~ 5x45=225 degrees"
+        "Battery 75%+, ~ 5x45=225 (5x50=250) degrees"
         )
         basic.showLeds(`
             . # # # .
@@ -550,6 +550,213 @@ input.onButtonPressed(Button.B, function () {
         quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
     }
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    quest_Note_3.quest_Show_String_For_Note_Big_Func(
+    "Approx. Target: 90deg CW/CCW (Your Bot may Differ)"
+    )
+    if (true) {
+        quest_Note_3.quest_Show_String_For_Note_Big_Func(
+        "Approx. Target: 90deg CW (Your Bot may Differ)"
+        )
+        quest_Note_3.quest_Show_String_For_Note_Small_Func(
+        "Slowest Turn: Pivot, Lo(30%) w/ Battery 75%+"
+        )
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Note_3.quest_Show_String_For_Note_Small_Func(
+        "More Closer to Target Angle: 2000 ms"
+        )
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_2000,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Note_3.quest_Show_String_For_Note_Small_Func(
+        "More Closer to Target Angle: 200 ms"
+        )
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_200,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Note_3.quest_Show_String_For_Note_Small_Func(
+        "More Closer to Target Angle: 40 ms"
+        )
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_40,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        quest_Note_3.quest_Show_String_For_Note_Big_Func(
+        "Approx. Target: 90deg CCW (Your Bot may Differ)"
+        )
+        quest_Note_3.quest_Show_String_For_Note_Small_Func(
+        "Slowest Turn: Pivot, Lo(30%) w/ Battery 75%+"
+        )
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Note_3.quest_Show_String_For_Note_Small_Func(
+        "More Closer to Target Angle: 2000 ms"
+        )
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_2000,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Note_3.quest_Show_String_For_Note_Small_Func(
+        "More Closer to Target Angle: 200 ms"
+        )
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_200,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Note_3.quest_Show_String_For_Note_Small_Func(
+        "More Closer to Target Angle: 40 ms"
+        )
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_40,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    quest_Note_3.quest_Show_String_For_Note_Big_Func(
+    "Approx.Target: 180deg CW/CCW (Your Bot may Differ)"
+    )
+    if (true) {
+        quest_Note_3.quest_Show_String_For_Note_Big_Func(
+        "Approx. Target: 180 deg CW (Your Bot may Differ)"
+        )
+        quest_Note_3.quest_Show_String_For_Note_Small_Func(
+        "Slowest Turn: Pivot, Lo(30%) w/ Battery 75%+"
+        )
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Note_3.quest_Show_String_For_Note_Small_Func(
+        "More Closer to Target Angle: 4000 ms"
+        )
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_4000,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Note_3.quest_Show_String_For_Note_Small_Func(
+        "More Closer to Target Angle: 400 ms"
+        )
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_400,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Note_3.quest_Show_String_For_Note_Small_Func(
+        "More Closer to Target Angle: 100 ms"
+        )
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_100,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        quest_Note_3.quest_Show_String_For_Note_Big_Func(
+        "Approx. Target: 180 deg CCW (Your Bot may Differ)"
+        )
+        quest_Note_3.quest_Show_String_For_Note_Small_Func(
+        "Slowest Turn: Pivot, Lo(30%) w/ Battery 75%+"
+        )
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Note_3.quest_Show_String_For_Note_Small_Func(
+        "More Closer to Target Angle: 4000 ms"
+        )
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_4000,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Note_3.quest_Show_String_For_Note_Small_Func(
+        "More Closer to Target Angle: 400 ms"
+        )
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_400,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Note_3.quest_Show_String_For_Note_Small_Func(
+        "More Closer to Target Angle: 100 ms"
+        )
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_100,
+        quest_Debug_Show_Enum.Dashboard_OLED
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    }
+})
+quest_Note_1.quest_Show_String_For_Note_Big_Func(
+"©️ 2024 Quest Institute. All rights reserved."
+)
 basic.showIcon(IconNames.Heart)
 quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
 basic.showIcon(IconNames.Happy)
